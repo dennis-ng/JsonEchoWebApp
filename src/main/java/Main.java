8c4b41583b4b0d2591649213718db499183c4500
Dennis <dennis.ng@u.nus.edu>
2016-03-10 00:01:50 +0800

    Cleaning up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,10 @@ public class Main {
 	public static void main(String[] args) {
 		Spark.port(Integer.valueOf(System.getenv("PORT")));
 		JSONObject obj = new JSONObject();
+		/**
+			When an "echo" request is received, a JSON string containing the
+			parameters and the current time will be generated and returned.
+		**/
 		Spark.get("echo", (request, response) -> {
 			obj.put("parameters", request.queryString());
 			obj.put("timestamp", new Date());
